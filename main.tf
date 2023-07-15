85c84d682923cef64559ae3614df2df601756615
Anshul Rastogi <ansrast@gmail.com>
2023-07-16 04:41:16 +0800

    added VPC configuration

--- main.tf
+++ main.tf
@@ -1,23 +1,141 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region = "us-west-2"
-}
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+  enable_dns_support = false
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "dev"
   }
+}
+
+resource "aws_subnet" "public1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "public1"
+  }
+}
+
+resource "aws_subnet" "public2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "public2"
+  }
+}
+
+resource "aws_subnet" "private1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.100.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "private1"
+  }
+}
+
+resource "aws_subnet" "private2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.101.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "private2"
+  }
+}
+
+resource "aws_internet_gateway" "public" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_eip" "nat1" {
+  vpc = true
+}
+
+resource "aws_eip" "nat2" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "main1" {
+  allocation_id = aws_eip.nat1.id
+  subnet_id     = aws_subnet.public1.id
+
+  tags = {
+    Name = "main1"
+  }
+}
+
+resource "aws_nat_gateway" "main2" {
+  allocation_id = aws_eip.nat2.id
+  subnet_id     = aws_subnet.public2.id
+
+  tags = {
+    Name = "main2"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.public.id
+  }
+
+  tags = {
+    name = "Public"
+  }
+}
+
+
+resource "aws_route_table" "private1" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main1.id
+
+  }
+
+  tags = {
+    name = "Private1"
+  }
+}
+
+resource "aws_route_table" "private2" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main2.id
+
+  }
+
+  tags = {
+    name = "Private2"
+  }
+}
+
+resource "aws_route_table_association" "public1" {
+  subnet_id       = aws_subnet.public1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public2" {
+  subnet_id       = aws_subnet.public2.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "priavte1" {
+  subnet_id       = aws_subnet.private1.id
+  route_table_id = aws_route_table.private1.id
+}
+
+resource "aws_route_table_association" "private2" {
+  subnet_id       = aws_subnet.private2.id
+  route_table_id = aws_route_table.private2.id
 }
